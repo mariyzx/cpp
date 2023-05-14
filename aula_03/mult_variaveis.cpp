a698a329c4bd2f2d535043b35afc77445150be5c
Mariana Werneck <marinhomariana8@gmail.com>
2023-05-14 13:50:32 -0300

    :sparkles: feat: declarações múltiplas de variáveis

---
+++ aula_03/mult_variaveis.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // diretiva
+
+using namespace std;
+
+#define pi 3.1415 // declara uma constante
+#define aluna cout << "Aluna Mariana Werneck" <<endl; // p imprimir o texto só chamar a const aluna
+
+int main() {
+
+/*   int vidas;
+  int tiros;
+  int life; */
+
+  int vidas=3, tiros=500, life=100;
+
+  cout << pi <<endl;
+  
+  aluna;
+
+  return 0;
+}
