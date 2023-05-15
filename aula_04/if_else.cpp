47dc6b503f2e38ef1fbbf68d911fdfedb83b223a
Mariana Werneck <marinhomariana8@gmail.com>
2023-05-15 18:26:30 -0300

    :sparkles: feat: if/else

---
+++ aula_04/if_else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num;
+
+  cout<<"Insira o valor de num"<<endl;
+  cin>>num;
+
+  if(num==1) {
+    cout<< "o valor de num é 1!";
+  } else {
+    cout<<"nao é 1 :(";
+  }
+
+  return 0;
+}
