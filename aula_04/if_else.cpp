d5ec8b305ccd4e6ba119483d305c11082843ff6a
Mariana Werneck <marinhomariana8@gmail.com>
2023-05-15 18:29:31 -0300

    :sparkles: feat: operador ternário

--- aula_04/if_else.cpp
+++ aula_04/if_else.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 int main() {
   int num;
+  string str;
 
   cout<<"Insira o valor de num"<<endl;
   cin>>num;
 
-  if(num==1) {
+  /* if(num==1) {
     cout<< "o valor de num é 1!";
   } else {
     cout<<"nao é 1 :(";
   }
+ */
+
+  str=num==1 ? "O valor de num é 1!" : "Não é 1 :(";
+  
+  cout << str << endl;
 
   return 0;
 }
