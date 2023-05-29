03fe997f26c49f364e855210f0c4b60edd791479
Mariana Werneck <marinhomariana8@gmail.com>
2023-05-29 19:59:34 -0300

    :sparkles: feat: adiciona exemplo da classe TProfessor

---
+++ TProfessor/TProfessor.cpp
@@ -0,0 +1,46 @@
+#include "iostream"
+#include "string.h"
+//#include<stdlib.h>
+
+using namespace std;
+
+class TProfessor{
+  private:
+    char nome[80];
+    int idade;
+  public:
+  // construtor
+    TProfessor(){
+      cout<<endl<<endl<<"     TProfessor"<<endl<<endl;
+      strcpy(nome,"");
+      idade = 0;
+    }
+  // destruidor
+    ~TProfessor(){
+      cout<<endl<<endl<<"     ~TProfessor"<<endl<<endl;
+    }
+    
+    void imprimeDados(void){
+       cout<<endl<<endl;
+       cout<<"     Nome  : "<<nome<<endl;
+       cout<<"     Idade : "<<idade<<endl;
+    }
+};
+
+
+void teste(void){
+  TProfessor prof2;
+}
+
+main(){
+  TProfessor prof1;
+
+  prof1.imprimeDados();
+
+  cout<<endl<<endl<<"     Antes da chamada a \"teste()\"..."<<endl<<endl;
+
+  teste();
+
+  cout<<endl<<endl<<"     Depois da chamada a \"teste()\"..."<<endl<<endl;
+
+}
