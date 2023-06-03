279d685cd22cd46af9adf3f6b3d68867f42ec17d
Mariana Werneck <marinhomariana8@gmail.com>
2023-06-03 20:12:16 -0300

    :sparkles: feat: adiciona classe ponto

---
+++ Trabalho_1/01_ponto.cpp
@@ -0,0 +1,32 @@
+// Questão 1: Desenvolva a classe Ponto, com os dados float x, y, e com os métodos void
+// entradaDeDados(void) e void impressaoDeDados(void), para a definição dos
+// valores de x e de y e para a impressão dos valores de x e de y, respectivamente.
+
+#include <iostream>
+
+using namespace std;
+
+class Ponto {
+  public:
+    float x,y;
+    void entradaDeDados(void);
+    void impressaoDeDados(void);
+};
+
+int main() {
+  Ponto ponto;
+  ponto.entradaDeDados();
+  ponto.impressaoDeDados();
+}
+
+
+void Ponto::entradaDeDados(void) {
+  cout<<"Insira o valor de x:"<<endl;
+  cin>>x;
+  cout<<"Insira o valor de y:"<<endl;
+  cin>>y;
+}
+
+void Ponto::impressaoDeDados(void) {
+  cout<<"O valor de x é "<<x<<" e o valor de y é "<<y<<"!"<<endl;
+}
